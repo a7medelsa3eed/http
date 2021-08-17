26bddb4dfce34880adadb47ada06c81e393f7821
ahmedelsayed <mosalahmoomoo@gmail.com>
2021-08-17 16:16:22 +0300

    fffgh
    hh

---
+++ lab8/lib/screens/userscreen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class UsersScreen extends StatefulWidget {
+  @override
+  _UsersScreenState createState() => _UsersScreenState();
+}
+
+class _UsersScreenState extends State<UsersScreen> {
+  bool loading = true;
+  List<User> users = [];
+
+  getUserList() async {
+    users = await UserService().getUsers();
+    loading = false;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getUserList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Users Screen"),
+      ),
+      body: loading
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: users.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: InkWell(
+                      onTap: () {
+                        pushPage(context, UserDetails(users[index]));
+                      },
+                      child: MyCard(content: "${users[index].name}")),
+                );
+              },
+            ),
+    );
+  }
+}
