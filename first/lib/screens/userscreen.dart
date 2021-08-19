4d02b0b751c0ad332223fc820ec39bb2f4719f47
ahmedelsayed <mosalahmoomoo@gmail.com>
2021-08-19 08:00:16 +0300

    map lab

---
+++ first/lib/screens/userscreen.dart
@@ -0,0 +1,56 @@
+
+import 'package:first/models/user.dart';
+import 'package:first/screens/userdetails.dart';
+import 'package:first/screens/widget/mycard.dart';
+import 'package:first/service/userservice.dart';
+import 'package:flutter/material.dart';
+
+import '../utils.dart';
+
+class UsersScreen extends StatefulWidget {
+  @override
+  _UsersScreenState createState() => _UsersScreenState();
+}
+
+class _UsersScreenState extends State<UsersScreen> {
+  bool loading = true;
+  List<User> users = [];
+
+  getUserList() async {
+    users = await UserService().getUsers();
+    loading = false;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getUserList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Users Screen"),
+      ),
+      body: loading
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: InkWell(
+                onTap: () {
+                  pushPage(context, UserDetails(users[index]));
+                },
+                child: MyCard(content: "${users[index].name}")),
+          );
+        },
+      ),
+    );
+  }
+}
+
+
---
+++ first/lib/service/userservice.dart
@@ -0,0 +1,19 @@
+import 'package:dio/dio.dart';
+import 'package:first/models/user.dart';
+
+class UserService {
+  String baseUrl = "https://jsonplaceholder.typicode.com/";
+  String users = "users";
+
+  Future<List<User>> getUsers() async {
+    List<User> usersList = [];
+    Dio dio = new Dio();
+    Response response = await dio.get("$baseUrl"+"$users");
+    var data = response.data;
+    data.forEach((userItem) {
+      User user = User.fromJson(userItem);
+      usersList.add(user);
+    });
+    return usersList;
+  }
+}
